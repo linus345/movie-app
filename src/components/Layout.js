a7ec3810c1a68256f1dbbeba02a2e261e5498f3c
linus345 <linusekman01@gmail.com>
2020-02-09 03:56:29 +0100

    added Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Layout = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: 70px 1fr;
+  grid-template-areas:
+    "sidebar x"
+    "sidebar main";
+`;
+
+export default Layout;
