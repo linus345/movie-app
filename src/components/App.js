0675866b6accace1b1c1ee584af1e8dfa169255e
linus345 <linusekman01@gmail.com>
2020-02-09 03:57:47 +0100

    changes to Sidebar component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
+import Layout from './Layout';
 import Sidebar from './Sidebar';
 
 const theme = {
@@ -11,15 +12,15 @@ const theme = {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
+      <Layout className="App">
         <Router>
+          <Sidebar />
           <Switch>
             <Route exact path="/" render={() => <p>home</p>} />
             <Route path="/credits" render={() => <p>credits</p>} />
           </Switch>
         </Router>
-        <Sidebar />
-      </div>
+      </Layout>
     </ThemeProvider>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Sidebar = () => {
   return (
     <StyledSidebar>
-      <p>Sidebar</p>
+      <div className="logo">
+        <h1>Movies</h1>
+      </div>
+      <div className="discover">
+        <NavLink to="/popular">Popular</NavLink>
+        <NavLink to="/top">Top</NavLink>
+        <NavLink to="/coming">Coming</NavLink>
+      </div>
+      <div className="genres">
+        <p>Popular genres</p>
+        <NavLink to="/action">Action</NavLink>
+        <NavLink to="/drama">Drama</NavLink>
+        <NavLink to="/scifi">Sci-Fi</NavLink>
+      </div>
     </StyledSidebar>
   );
 }
 
 const StyledSidebar = styled.aside`
-  color: red;
+  grid-area: sidebar;
+  min-height: 100%;
+  display: grid;
+  grid-template-rows: 70px 1fr 1fr;
 `;
 
 export default Sidebar;
