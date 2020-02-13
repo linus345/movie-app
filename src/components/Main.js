5538af0a916a5fd36f0254df423457d819f32163
linus345 <linusekman01@gmail.com>
2020-02-14 00:39:08 +0100

    added padding to top and bottom

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const Main = styled.main`
   grid-area: main;
+  padding: 70px 60px;
   overflow-y: scroll;
-  padding: 0px 60px;
 `;
 
 export default Main;
