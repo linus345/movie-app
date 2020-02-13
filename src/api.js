dc20bc4330aa8a99af798921e030ade1f07a532f
linus345 <linusekman01@gmail.com>
2020-02-14 00:20:00 +0100

    added temporary base url for images. Will eventually get fetched dynamically from backend

--- src/api.js
+++ src/api.js
@@ -4,6 +4,8 @@ const instance = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
+export const imageBase = "https://image.tmdb.org/t/p";
+
 export const getPopular = () => {
   return instance.get("/popular");
 }
