1838002ecbedd7a340b1cadd323b3810982b3cd2
linus345 <linusekman01@gmail.com>
2020-03-28 13:33:57 +0100

    added play button to watch the trailer on youtube

--- src/components/MovieDetailsGrid.js
+++ src/components/MovieDetailsGrid.js
@@ -6,16 +6,6 @@ const MovieDetailsGrid = styled.div`
   grid-template-rows: minmax(min-content, max-content) auto 2fr;
   width: 100%;
   max-width: 100%;
-
-  .movie-poster {
-    position: absolute;
-    grid-row: 1 / span 2;
-    width: 100%;
-    height: 100%;
-    background-image: url("${props => props.img}");
-    background-size: cover;
-    background-position: center;
-  }
 `;
 
 export default MovieDetailsGrid;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useRef } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon as FaIcon } from '@fortawesome/react-fontawesome';
+import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 import * as api from '../api';
@@ -16,11 +17,13 @@ const Movie = ({ location, history }) => {
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState(null);
+  const trailerRef = useRef(null);
 
   const getMovie = async (movieId) => {
     try {
       const res = await api.getMovie(movieId);
       console.log("res: ", res);
+      trailerRef.current = getTrailer(res.data.videos.results);
       setMovie(res.data);
       setLoading(false);
     } catch(err) {
@@ -36,6 +39,18 @@ const Movie = ({ location, history }) => {
     }
   }
 
+  const getTrailer = videos => {
+    console.log("videos: ", videos);
+    let video = null;
+    for (let i = 0; i < videos.length; i++) {
+      if (videos[i].site === "YouTube" && videos[i].type === "Trailer") {
+        video = videos[i];
+        break;
+      }
+    }
+    return video;
+  }
+
   useEffect(() => {
     getMovie(movieId);
   }, []);
@@ -48,15 +63,53 @@ const Movie = ({ location, history }) => {
         <FaIcon icon={faArrowLeft} />
         <p>Back</p>
       </Button>
-      <MovieDetailsGrid img={`${api.imageBase}/w1280/${movie.backdrop_path}`}>
-        {/* css for movie-poster is in MovieDetailsGrid.js for now */}
-        {/* TODO: Add play button on poster to play trailer */}
-        <div className="movie-poster"></div>
+      <MovieDetailsGrid>
+        <MoviePoster
+          img={`${api.imageBase}/w1280/${movie.backdrop_path}`}
+        >
+          {trailerRef.current && (
+            <a
+              href={`https://youtube.com/watch?v=${trailerRef.current.key}`}
+              target="_blank"
+              className="play-button"
+            >
+              <FaIcon icon={faPlayCircle} style={{ color: "white" }} />
+            </a>
+          )}
+        </MoviePoster>
         <MovieShortInfo movie={movie} />
         <MovieTabs movie={movie} />
       </MovieDetailsGrid>
     </Fragment>
   );
 }
+
+const MoviePoster = styled.div`
+  position: absolute;
+  grid-row: 1 / span 2;
+  width: 100%;
+  height: 100%;
+  background-image: url("${props => props.img}");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .play-button {
+    z-index: 10;
+    border: none;
+    background-color: rgba(0, 0, 0, 0);
+    color: white;
+    text-decoration: none;
+    font-size: 70px;
+    cursor: pointer;
+    transition: transform 0.3s;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
 
 export default Movie;
